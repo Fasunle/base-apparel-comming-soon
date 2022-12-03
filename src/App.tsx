127029cab16f718e0d0ccc208952c95e0266bb7b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-12-03 20:29:30 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className='app'>
+      We're coming soon Hello fellow shoppers! We're currently building our new
+      fashion store. Add your email below to stay up-to-date with announcements
+      and our launch deals. Email Address
+    </div>
+  );
+};
+
+export default App;
